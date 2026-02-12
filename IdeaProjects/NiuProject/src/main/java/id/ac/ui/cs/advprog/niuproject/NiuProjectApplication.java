ec721a53eeae8e215c1026af944a70134892621b
manpas-thebreathinghuman <rajafadhlurrohman@gmail.com>
2026-02-12 14:13:19 +0700

    idk first

---
+++ IdeaProjects/NiuProject/src/main/java/id/ac/ui/cs/advprog/niuproject/NiuProjectApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.niuproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiuProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NiuProjectApplication.class, args);
+    }
+
+}
---
+++ IdeaProjects/NiuProject/src/test/java/id/ac/ui/cs/advprog/niuproject/NiuProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.niuproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiuProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
