4903f2ec5cc99de0a3eb4875c5679159750ad829
manpas-thebreathinghuman <rajafadhlurrohman@gmail.com>
2026-02-22 22:07:14 +0700

    ProductRepoTest

--- IdeaProjects/NiuProject/build.gradle.kts
+++ IdeaProjects/NiuProject/build.gradle.kts
@@ -1,7 +1,7 @@
-val seleniumJavaVersion = "4.14.1"
+val seleniumJavaVersion = "4.31.0"
 val seleniumJupiterVersion = "5.0.1"
 val webdrivermanagerVersion = "5.6.3"
-val junitJupiterVersion = "5.9.1"
+val junitJupiterVersion = "5.12.2"
 
 plugins {
     java
@@ -38,10 +38,10 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    testImplementation("org.seleniumhg.selenium:selenium-java:$seleniumJavaVersion")
-    testImplementation("io.github.bonigarcia:selenuim-jupiter:$seleniumJupiterVersion")
+    testImplementation("org.seleniumhq.selenium:selenium-java:$seleniumJavaVersion")
+    testImplementation("io.github.bonigarcia:selenium-jupiter:$seleniumJupiterVersion")
     testImplementation("io.github.bonigarcia:webdrivermanager:$webdrivermanagerVersion")
-    testImplementation("io.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
 }
 
